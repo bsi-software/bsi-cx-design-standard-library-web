7fbc24932015f803a323f186bf78ca87e962d279
Lukas Steiger <lukas.steiger@bsi-software.com>
2023-08-11 15:38:40 +0200

    #358866 - always use latest flags - prevent import issues

--- content-elements/form/form-field-tel/prototype/form-tel-input.js
+++ content-elements/form/form-field-tel/prototype/form-tel-input.js
@@ -1,5 +1,5 @@
-import intlTelInput from 'intl-tel-input'
-import 'intl-tel-input/build/css/intlTelInput.css'
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
 import Alpine from "alpinejs";
 
 Alpine.data('telInput', () => ({
