f9c77c19f5b89b9ab6d0bb0fc23cf0e854ef03e6
Lukas Steiger <lukas.steiger@bsi-software.com>
2023-08-11 09:50:37 +0200

    #352221 - fix chart color access

--- content-elements/base/chart/prototype/chart.js
+++ content-elements/base/chart/prototype/chart.js
@@ -1,6 +1,5 @@
 import { ChartConfig, ChartUrlProvider } from '@bsi-cx/chart';
 import ChartConfigColor from '@bsi-cx/chart/src/config/color';
-import { bsiProperty } from '@bsi-cx/design-build';
 import Alpine from 'alpinejs';
 import 'chart.js/auto';
 
@@ -14,6 +13,12 @@ Alpine.data('chartElement', () => ({
 
   initChart() {
     this.chart = this.$el;
+
+    const borderColors = this.chart.dataset.bordercolors.split(",");
+    const backgroundColors = this.chart.dataset.backgroundcolors.split(",");
+    for(let i = 0; i < Math.min(borderColors.length, backgroundColors.length); i++) {
+      colors[i] = ChartConfigColor.of(borderColors[i], backgroundColors[i]);
+    }
 
     this.config = new ChartConfig()
       .withConfigPostProcessor(this.chartPreprocessor)
@@ -38,15 +43,5 @@ Alpine.data('chartElement', () => ({
   },
 }))
 
-/*const colors = [
-  ChartConfigColor.of('#116985ff','#11698566'),
-  ChartConfigColor.of( '#27aab0ff','#27aab066'),
-  ChartConfigColor.of( '#94e7a8ff', '#94e7a866'),
-  ChartConfigColor.of( '#c7f296ff','#c7f29666')
-]*/
-const colors = [
-    ChartConfigColor.of(bsiProperty('chartBorderColor1', '#116985ff'),bsiProperty('chartBackgroundColor1', '#11698566')),
-    ChartConfigColor.of(bsiProperty('chartBorderColor2', '#27aab0ff'),bsiProperty('chartBackgroundColor2', '#27aab066')),
-    ChartConfigColor.of(bsiProperty('chartBorderColor3', '#94e7a8ff'),bsiProperty('chartBackgroundColor3', '#94e7a866')),
-    ChartConfigColor.of(bsiProperty('chartBorderColor4', '#c7f296ff'),bsiProperty('chartBackgroundColor4', '#c7f29666'))
-];
+// @bsi-cx/chart requires us to keep thisglobal
+const colors = [];
