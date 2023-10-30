280c3e75ab760eb17a14f8a331e00f03b8c7aa03
Robin Christen <robin.christen@bsi-software.com>
2023-10-30 18:31:24 +0100

    #350873 use flatpickr, remove vanilla-calendar dependency

--- content-elements/base/slot-finder/src/api.js
+++ content-elements/base/slot-finder/src/api.js
@@ -1,26 +1,8 @@
-import { toLocalISOStringWithoutTAndZ } from "./utils/date-utils";
+import { getFirstDayOfMonth, getLastDayOfMonth, toLocalISOStringWithoutTAndZ } from "./utils/date-utils";
 
 /**
  * @typedef {import("./slot/slot").Slot} Slot
  */
-
-/**
- * Fetch slots for the given date range
- *
- * @param {String} url
- * @param {Date} fromDate
- * @param {Date} toDate
- * @returns {Promise<Slot[]>} slots
- */
-export async function fetchSlotsInRange(url, fromDate, toDate) {
-  const from = toLocalISOStringWithoutTAndZ(fromDate);
-  const to = toLocalISOStringWithoutTAndZ(toDate);
-  const data = await fetch(url + "&" + new URLSearchParams({ from, to }), {
-    headers: { Accept: "application/json" },
-  });
-  const json = await data.json();
-  return json.timeslots?.map(convertSlot) ?? [];
-}
 
 /**
  * Book the selected slot
@@ -46,6 +28,35 @@ export async function bookSlot(url, slot) {
   document.body.appendChild(form);
 
   form.submit();
+}
+
+/**
+ * Fetch slots of the given month
+ *
+ * @param {String} url
+ * @param {Date} month
+ * @returns {Promise<Slot[]>} slots
+ */
+export async function fetchSlotsOfMonth(url, month) {
+  return fetchSlotsInRange(url, getFirstDayOfMonth(month), getLastDayOfMonth(month));
+}
+
+/**
+ * Fetch slots for the given date range
+ *
+ * @param {String} url
+ * @param {Date} fromDate
+ * @param {Date} toDate
+ * @returns {Promise<Slot[]>} slots
+ */
+async function fetchSlotsInRange(url, fromDate, toDate) {
+  const from = toLocalISOStringWithoutTAndZ(fromDate);
+  const to = toLocalISOStringWithoutTAndZ(toDate);
+  const data = await fetch(url + "&" + new URLSearchParams({ from, to }), {
+    headers: { Accept: "application/json" },
+  });
+  const json = await data.json();
+  return json.timeslots?.map(convertSlot) ?? [];
 }
 
 /**
--- content-elements/base/slot-finder/src/calendar.js
+++ content-elements/base/slot-finder/src/calendar.js
@@ -1,17 +1,9 @@
-import VanillaCalendar from "@uvarov.frontend/vanilla-calendar";
-import { changeMonth, changeYear, changeYearAndMonth, getDaysOfMonth, getFirstDayOfMonth, toLocalISOStringDate } from "./utils/date-utils.js";
-import { fetchSlotsInRange } from "./api.js";
-
-// Basic styles
-import "@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css";
-
-// Additional styles
-import "@uvarov.frontend/vanilla-calendar/build/themes/light.min.css";
-import "@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/l10n/de.js";
+import "flatpickr/dist/l10n/fr.js";
+import "flatpickr/dist/l10n/it.js";
+import { fetchSlotsOfMonth } from "./api.js";
 import { isSlotOnDay } from "./slot/slot.js";
-
-/** @type {Date} */
-let selectedMonth = getFirstDayOfMonth();
 
 /**
  * Init calendar
@@ -20,48 +12,27 @@ let selectedMonth = getFirstDayOfMonth();
  * @param {HTMLElement} $element
  */
 export function initCalendar(model, $element) {
-  const calendar = new VanillaCalendar($element, {
-    settings: {
-      lang: model.lang,
-      visibility: {
-        theme: "light",
-        daysOutside: false,
-      },
-      range: {
-        disablePast: true,
-        disableAllDays: true,
-      },
+  $element.innerHTML = ''; // remove static flatpickr instance used in content-editor template
+  const calendar = flatpickr($element, {
+    locale: model.lang,
+    inline: true,
+    minDate: 'today',
+    enable: [],
+    onChange: dates => {
+      if (dates && dates.length > 0) {
+        model.selectedDate.set(new Date(dates[0]));
+      }
     },
-    actions: {
-      clickDay: (_, dates) => {
-        if (dates && dates.length > 0) {
-          model.selectedDate.set(new Date(dates[0]));
-        }
-      },
-      clickMonth: (_, month) => setMonth(changeMonth(selectedMonth, month)),
-      clickYear: (_, year) => setMonth(changeYear(selectedMonth, year)),
-      clickArrow: (_, year, month) => setMonth(changeYearAndMonth(selectedMonth, year, month)),
-    },
+    onMonthChange: () => fetchSlots(),
+    onYearChange: () => fetchSlots(),
   });
 
-  /**
-   * @param {Date} month 
-   */
-  async function setMonth(month) {
-    calendar.settings.range.disableAllDays = true;
-    calendar.update();
-
-    const slots = await fetchSlotsInRange(model.url, month, changeMonth(month, month.getMonth() + 1));
-    const daysWithoutSlots = getDaysOfMonth(month)
-      .filter(day => !slots.some(slot => isSlotOnDay(slot, day)));
-
-    calendar.settings.range.disabled = daysWithoutSlots.map(toLocalISOStringDate);
-    calendar.settings.range.disableAllDays = false;
-    calendar.update();
-
+  async function fetchSlots() {
+    const month = new Date(calendar.currentYear, calendar.currentMonth);
+    const slots = await fetchSlotsOfMonth(model.url, month);
+    calendar.set("enable", [day => slots.some(slot => isSlotOnDay(slot, day))]);
     model.slots.set(slots);
   }
 
-  calendar.init();
-  setMonth(getFirstDayOfMonth());
+  fetchSlots().then(() => calendar.setDate(model.selectedDate.get()));
 }
