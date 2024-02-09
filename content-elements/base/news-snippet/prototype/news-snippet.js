e6644bc53c34374ba36678366e8c111beae2791a
Lukas Steiger <lukas.steiger@bsi-software.com>
2024-02-09 18:41:44 +0100

    fix table and news-snippet

--- content-elements/base/news-snippet/prototype/news-snippet.js
+++ content-elements/base/news-snippet/prototype/news-snippet.js
@@ -3,13 +3,14 @@ import Alpine from '@alpinejs/csp';
 Alpine.data('newsSnippet', () => ({
 
   // Set href of button
-  setButtonUrl(el) {
+  setButtonUrl() {
+    const cardElement = this.$el;
     let url = '';
-    if (el.firstElementChild.firstElementChild) {
-      url = el.firstElementChild.firstElementChild.href;
-      el.getElementsByClassName("bsi-news-btn")[0].href = url;
+    if (cardElement.firstElementChild.firstElementChild) {
+      url = cardElement.firstElementChild.firstElementChild.href;
+      cardElement.getElementsByClassName("bsi-news-btn")[0].href = url;
     } else {
-      el.getElementsByClassName("bsi-news-btn")[0].style.display = "none";
+      cardElement.getElementsByClassName("bsi-news-btn")[0].style.display = "none";
     }
   },
 }))
