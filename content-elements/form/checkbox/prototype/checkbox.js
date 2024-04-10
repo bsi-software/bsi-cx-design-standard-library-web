ec47d63178c5d9b7ea4b53ea3fc439c5c9d43d93
Sophia Wild <sophia.wild@bsi-software.com>
2024-04-10 15:18:40 +0200

    add checkbox validation

---
+++ content-elements/form/checkbox/prototype/checkbox.js
@@ -0,0 +1,17 @@
+import Alpine from '@alpinejs/csp';
+
+Alpine.data('checkbox', () => ({
+    requiredErrorElement: null,
+
+    initRequiredError() {
+        this.requiredErrorElement = this.$el;
+    },
+
+    validateInput() {
+        if (!this.inputEl.value && this.inputEl.hasAttribute('required')) {
+        this.requiredErrorElement.style.display = "block";
+        } else {
+        this.requiredErrorElement.style.display = "none";
+        }
+    },
+}))
