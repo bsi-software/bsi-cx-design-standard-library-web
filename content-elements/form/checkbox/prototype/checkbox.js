c39ab69c23f4bfd5946b7c14157bea4d3b32ff2a
Sophia Wild <sophia.wild@bsi-software.com>
2024-04-11 13:29:20 +0200

    add x-data

--- content-elements/form/checkbox/prototype/checkbox.js
+++ content-elements/form/checkbox/prototype/checkbox.js
@@ -1,17 +1,17 @@
 import Alpine from '@alpinejs/csp';
 
 Alpine.data('checkbox', () => ({
-    requiredErrorElement: null,
+  requiredErrorElement: null,
 
-    initRequiredError() {
-        this.requiredErrorElement = this.$el;
-    },
+  initRequiredError() {
+    this.requiredErrorElement = this.$el;
+  },
 
-    validateInput() {
-        if (!this.inputEl.value && this.inputEl.hasAttribute('required')) {
-        this.requiredErrorElement.style.display = "block";
-        } else {
-        this.requiredErrorElement.style.display = "none";
-        }
-    },
+  validateInput() {
+    if (!this.inputEl.value && this.inputEl.hasAttribute('required')) {
+      this.requiredErrorElement.style.display = "block";
+    } else {
+      this.requiredErrorElement.style.display = "none";
+    }
+  },
 }))
