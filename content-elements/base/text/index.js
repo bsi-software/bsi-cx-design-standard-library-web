51cdfbd8432cecc1cc797b6cd16ec12d97988404
derB4 <122090401+derB4@users.noreply.github.com>
2024-09-06 12:06:06 +0200

    377652: Allow custom Text Editor (#128)

--- content-elements/base/text/index.js
+++ content-elements/base/text/index.js
@@ -1,3 +1,12 @@
 require('./styles.scss');
 
 module.exports = require('./prototype')();
+module.exports.editor = (textEditorConfig) =>
+  require('./prototype')(
+    require('./template.twig'),
+    'text-kkq2fq',
+    'Text',
+    'text-part-Fjse59',
+    'Text',
+    textEditorConfig
+  );
