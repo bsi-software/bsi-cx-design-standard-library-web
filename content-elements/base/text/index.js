7db86ed2259375b536cebec1efccc053eeab5a0b
Lukas Heyer <lukas.heyer@bsi-software.com>
2024-10-11 09:37:50 +0200

    Revert 377652: Extended Text Editor

--- content-elements/base/text/index.js
+++ content-elements/base/text/index.js
@@ -1,12 +1,3 @@
 require('./styles.scss');
 
 module.exports = require('./prototype')();
-module.exports.editor = (textEditorConfig) =>
-  require('./prototype')(
-    require('./template.twig'),
-    'text-kkq2fq',
-    'Text',
-    'text-part-Fjse59',
-    'Text',
-    textEditorConfig
-  );
