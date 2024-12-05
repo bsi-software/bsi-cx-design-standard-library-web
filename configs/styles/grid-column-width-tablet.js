00f66cc581ada3dbb4eec8a1ac5153daeb13b0db
kno-bsi <keri.nogarth@bsi-software.com>
2024-12-05 19:34:57 +0100

    add: new bootstrap css grid layout element

---
+++ configs/styles/grid-column-width-tablet.js
@@ -0,0 +1,31 @@
+const {cx} = require('@bsi-cx/design-build');
+
+module.exports = cx.style
+  .withIdentifier('grid-col-width-tablet-578494b3')
+  .withLabel('Spaltenbreiten Tablet')
+  .withCssClasses(
+    cx.cssClass
+      .withLabel('1/8')
+      .withCssClass('g-col-md-1'),
+    cx.cssClass
+      .withLabel('2/8')
+      .withCssClass('g-col-md-2'),
+    cx.cssClass
+      .withLabel('3/8')
+      .withCssClass('g-col-md-3'),
+    cx.cssClass
+      .withLabel('4/8')
+      .withCssClass('g-col-md-4'),
+    cx.cssClass
+      .withLabel('5/8')
+      .withCssClass('g-col-md-5'),
+    cx.cssClass
+      .withLabel('6/8')
+      .withCssClass('g-col-md-6'),
+    cx.cssClass
+      .withLabel('7/8')
+      .withCssClass('g-col-md-7'),
+    cx.cssClass
+      .withLabel('8/8')
+      .withCssClass('g-col-md-8'),
+    );
