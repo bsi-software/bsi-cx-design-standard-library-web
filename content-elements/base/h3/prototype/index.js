6b99e6d7c786884898ab12bba70da0eb5ff1a63b
LukasBSI <120456416+LukasBSI@users.noreply.github.com>
2025-02-05 14:43:19 +0100

    Headlines: add tooltip focus, cleanup checkbox, update dependencies (#160)

--- content-elements/base/h3/prototype/index.js
+++ content-elements/base/h3/prototype/index.js
@@ -10,12 +10,12 @@ const {cx, Icon} = require('@bsi-cx/design-build');
  */
 module.exports = (
   template = require('../template.twig'),
-  elementId = 'title-h2-KNs10G',
-  /*elementLabel = 'Heading H2',*/
-  elementLabel = 'H2 Überschrift',
-  titlePartId = 'title-h2-part-vD6omt',
-  /*titlePartLabel = 'Heading H2'*/
-  titlePartLabel = 'H2 Überschrift'
+  elementId = 'title-h3-gbnkTT',
+  /*elementLabel = 'Heading H3',*/
+  elementLabel = 'H3 Überschrift',
+  titlePartId = 'title-h3-part-mJ0yli',
+  /*titlePartLabel = 'Heading H3'*/
+  titlePartLabel = 'H3 Überschrift'
 ) => cx
   .contentElement
   .withFile(template)
@@ -23,9 +23,9 @@ module.exports = (
   .withLabel(elementLabel)
   .withIcon(Icon.TEXT)
   .withStyleConfigs(
-    require('../../../../../configs/styles/text-color'),
-    require('../../../../../configs/styles/text-weight'),
-    require('../../../../../configs/styles/headline-style'))
+    require('../../../../configs/styles/text-color'),
+    require('../../../../configs/styles/text-weight'),
+    require('../../../../configs/styles/headline-style'))
   .withParts(
     cx.part.plainText
       .withId(titlePartId)
