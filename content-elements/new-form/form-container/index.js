812aff8fcbc59169901de9d37bec9e9aac7c5274
kno-bsi <keri.nogarth@bsi-software.com>
2025-06-30 10:28:56 +0200

    WIP

---
+++ content-elements/new-form/form-container/index.js
@@ -0,0 +1,31 @@
+require("./styles.scss");
+const { cx, Icon } = require("@bsi-cx/design-build");
+
+const element = cx.contentElement;
+
+/**
+ * @returns {ContentElement}
+ */
+module.exports = element;
+element
+  .withFile(require("./template.twig"))
+  .withElementId("form-container-84c6581f")
+  /*.withLabel('Form')*/
+  .withLabel("Neues Formular")
+  .withIcon(Icon.ONE_COLUMN)
+  .withStyleConfigs(
+    require("../../../configs/styles/form-color-scheme.js"),
+  )
+  .withParts(
+    cx.part.form
+      .withId("form-container-part-06acb819")
+      /*.withLabel('Form'))*/
+      .withLabel("Formular Einstellungen")
+  )
+  .withDropzones(
+    cx.dropzone
+      .withDropzone("form-container-dropdown-cd1f258c")
+      .withAllowedElements(
+        require("../form-section")
+      ),
+  );
