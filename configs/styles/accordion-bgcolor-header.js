2cb0f26b0eae9a30b747d60b617fa373ed2b3efd
Antonino Del Fiume <antonino.delfiume@bsi-software.com>
2025-08-14 14:47:20 +0200

    Add MR Fixes

--- configs/styles/accordion-bgcolor-header.js
+++ configs/styles/accordion-bgcolor-header.js
@@ -2,21 +2,21 @@ const { cx } = require("@bsi-cx/design-build");
 
 module.exports = cx.style
     .withIdentifier("accordion-head-background-b72812")
-    .withLabel("Accordion head background color")
+    .withLabel("Hintergrundfarbe Kopf")
     .withCssClasses(
         cx.cssClass
-        .withLabel("Primary")
-        .withCssClass("bg-acc-primary"),
+        .withLabel("Primärfarbe")
+        .withCssClass("bg-acc-primary-color"),
         cx.cssClass
-        .withLabel("Secondary")
-        .withCssClass("bg-acc-secondary"),
+        .withLabel("Sekundärfarbe")
+        .withCssClass("bg-acc-secondary-color"),
         cx.cssClass
-        .withLabel("Dark color")
+        .withLabel("Dunkel")
         .withCssClass("bg-acc-dark-color"),
         cx.cssClass
-        .withLabel("Light color")
+        .withLabel("Hell")
         .withCssClass("bg-acc-light-color"),
         cx.cssClass
-        .withLabel("Info color")
+        .withLabel("Info Farbe")
         .withCssClass("bg-acc-info-color")
     );
--- configs/styles/accordion-text-color-header.js
+++ configs/styles/accordion-text-color-header.js
@@ -2,21 +2,21 @@ const { cx } = require("@bsi-cx/design-build");
 
 module.exports = cx.style
     .withIdentifier("accordion-head-txt-f60902")
-    .withLabel("Accordion head text color")
+    .withLabel("Textfarbe Kopf")
     .withCssClasses(
         cx.cssClass
-        .withLabel("Primary")
-        .withCssClass("txt-acc-primary"),
+        .withLabel("Primärfarbe")
+        .withCssClass("txt-acc-primary-color"),
         cx.cssClass
-        .withLabel("Secondary")
-        .withCssClass("txt-acc-secondary"),
+        .withLabel("Sekundärfarbe")
+        .withCssClass("txt-acc-secondary-color"),
         cx.cssClass
-        .withLabel("Dark color")
+        .withLabel("Dunkel")
         .withCssClass("txt-acc-dark-color"),
         cx.cssClass
-        .withLabel("Light color")
+        .withLabel("Hell")
         .withCssClass("txt-acc-light-color"),
         cx.cssClass
-        .withLabel("Info color")
+        .withLabel("Info Farbe")
         .withCssClass("txt-acc-info-color")
     );
