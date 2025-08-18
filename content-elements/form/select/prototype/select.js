28ff14286f370956a85ee107745d6a07d655206e
Antonino Del Fiume <antonino.delfiume@bsi-software.com>
2025-08-18 17:25:06 +0200

    Add MR fix

--- content-elements/form/select/prototype/select.js
+++ content-elements/form/select/prototype/select.js
@@ -8,15 +8,10 @@ Alpine.data("selectField", () => ({
     this.rootElement = this.$root;
     this.selectElement = this.$el;
     const selectedOption = this.selectElement.options[this.selectElement.selectedIndex].value;
-
     
-
     if (this.rootElement.classList.contains("bsi-placeholder-defined")) {
       if(selectedOption === this.selectElement.options[0].value) {
         this.selectElement.options[0].setAttribute("selected", "");
-        // The select field was initialized with an empty value for the placeholder. We need to quickly toggle the value for the required value to be properly handled by the form.
-        this.selectElement.value = this.selectElement.options[0].value + "_temp";
-        this.selectElement.value = this.selectElement.options[0].value;
       }
       this.selectElement.options[0].setAttribute("value", "");
     }
