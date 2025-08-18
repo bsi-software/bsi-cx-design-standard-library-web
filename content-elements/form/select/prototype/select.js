f242b7d42a9ebf147c7847972b585d09ffa9bd74
Antonino Del Fiume <antonino.delfiume@bsi-software.com>
2025-08-18 16:00:37 +0200

    Add fix on select element

--- content-elements/form/select/prototype/select.js
+++ content-elements/form/select/prototype/select.js
@@ -1,21 +1,28 @@
-import Alpine from '@alpinejs/csp';
+import Alpine from "@alpinejs/csp";
 
-Alpine.data('selectField', () => ({
+Alpine.data("selectField", () => ({
   rootElement: null,
   selectElement: null,
 
   initSelectField() {
     this.rootElement = this.$root;
     this.selectElement = this.$el;
+    const selectedOption = this.selectElement.options[this.selectElement.selectedIndex].value;
 
-    if (this.rootElement.classList.contains('bsi-placeholder-defined')) {
+    
 
-      this.selectElement.options[0].setAttribute('value', '');
-      this.selectElement.options[0].setAttribute('selected', '');
+    if (this.rootElement.classList.contains("bsi-placeholder-defined")) {
+      this.selectElement.options[0].setAttribute("value", "");
 
-      // The select field was initialized with an empty value for the placeholder. We need to quickly toggle the value for the required value to be properly handled by the form.
-      this.selectElement.value = this.selectElement.options[0].value + '_temp';
-      this.selectElement.value = this.selectElement.options[0].value;
+      if(selectedOption === this.selectElement.options[0].value) {
+        this.selectElement.options[0].setAttribute("selected", "");
+        // The select field was initialized with an empty value for the placeholder. We need to quickly toggle the value for the required value to be properly handled by the form.
+        this.selectElement.value = this.selectElement.options[0].value + "_temp";
+        this.selectElement.value = this.selectElement.options[0].value;
+      }
+
+
     }
+
   },
-}))
+}));
