094d8f8d3f2c1855f7e1f75c3e756f1a60c51390
Benni Vier <benjamin.vier@bsi-software.com>
2025-08-22 18:59:15 +0200

    Regex Element

--- configs/styles/form-field-pattern.js
+++ configs/styles/form-field-pattern.js
@@ -1,18 +1,20 @@
-const { cx, bsiProperty } = require('@bsi-cx/design-build');
+const { cx, bsiProperty, Version } = require('@bsi-cx/design-build');
 
-// Example: JSON.stringify({ "Keiner": "", "PLZ": "[0-9]{5}", "KFZ": "[A-ZÖÜÄ]{1,3} [A-ZÖÜÄ]{1,2} [1-9]{1}[0-9]{1,3}@" })
+// Example: JSON.stringify({ "Keiner": "", "PLZ": "[0-9]{5}", "KFZ": "[A-ZÖÜÄ]{1,3} [A-ZÖÜÄ]{1,2} [1-9]{1}[0-9]{1,3}" })
 const regexPatterns = JSON.parse(bsiProperty('regexPattern', '{}'));
 
 
 module.exports = regexPatterns && Object.keys(regexPatterns).length ? cx.style
   .withIdentifier('form-field-pattern-wJakrq')
+  .withMinVersion(Version.CX_23_1)
   /*.withLabel('Regex validation')*/
   .withLabel('Regex Validierung')
   .withStyleOptions(
     ...Object.entries(regexPatterns)
       .map(([key, value]) => {
-        let replacedValue = String(value).replaceAll('[', '\\[').replaceAll('{', '\\{');
-        return cx.styleOption.withLabel(key).withStyleId(key.toLowerCase()).withCssClass(key.toLowerCase())
+        let replacedValue = String(value).replaceAll(/([\\d|\\s|\[|\{|])/gm, "\\$1");
+        let id = key.toLocaleLowerCase().replaceAll(' ', '');
+        return cx.styleOption.withLabel(key).withStyleId(id).withCssClass(id)
           .withDomManipulations(
             cx.domManipulation
               .withSelector('input')
