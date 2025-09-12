ef1c4fc2ac96c4bf7b414a2037b5d711d8289e90
kno-bsi <keri.nogarth@bsi-software.com>
2025-09-12 13:28:17 +0200

    WIP not working

--- content-elements/new-form/form-container/index.js
+++ content-elements/new-form/form-container/index.js
@@ -1,33 +1,3 @@
-require("./styles.scss");
-const { cx, Icon } = require("@bsi-cx/design-build");
+require('./styles.scss');
 
-const element = cx.contentElement;
-
-/**
- * @returns {ContentElement}
- */
-module.exports = element;
-element
-  .withFile(require("./template.twig"))
-  .withElementId("form-container-84c6581f")
-  /*.withLabel('Form')*/
-  .withLabel("Neues Formular")
-  .withIcon(Icon.ONE_COLUMN)
-  .withStyleConfigs(
-    require("../../../configs/styles/form-color-scheme.js"),
-    require("../../../configs/styles/form-button-alignment.js"),
-
-  )
-  .withParts(
-    cx.part.form
-      .withId("form-container-part-06acb819")
-      /*.withLabel('Form'))*/
-      .withLabel("Formular Einstellungen")
-  )
-  .withDropzones(
-    cx.dropzone
-      .withDropzone("form-container-dropdown-cd1f258c")
-      .withAllowedElements(
-        require("../form-section")
-      ),
-  );
+module.exports = require('./prototype')();
