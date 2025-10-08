bf84a2e8c53ea6a73d98729c08cd274a5a259833
kno-bsi <keri.nogarth@bsi-software.com>
2025-10-08 07:42:40 +0200

    refactor: grid-breakpoint

--- configs/styles/grid-column-width-desktop.js
+++ configs/styles/grid-column-width-desktop.js
@@ -6,38 +6,38 @@ module.exports = cx.style
   .withCssClasses(
     cx.cssClass
       .withLabel('1/12')
-      .withCssClass('g-col-xl-1'),
+      .withCssClass('g-col-xxl-1'),
     cx.cssClass
       .withLabel('2/12')
-      .withCssClass('g-col-xl-2'),
+      .withCssClass('g-col-xxl-2'),
     cx.cssClass
       .withLabel('3/12')
-      .withCssClass('g-col-xl-3'),
+      .withCssClass('g-col-xxl-3'),
     cx.cssClass
       .withLabel('4/12')
-      .withCssClass('g-col-xl-4'),
+      .withCssClass('g-col-xxl-4'),
     cx.cssClass
       .withLabel('5/12')
-      .withCssClass('g-col-xl-5'),
+      .withCssClass('g-col-xxl-5'),
     cx.cssClass
       .withLabel('6/12')
-      .withCssClass('g-col-xl-6'),
+      .withCssClass('g-col-xxl-6'),
     cx.cssClass
       .withLabel('7/12')
-      .withCssClass('g-col-xl-7'),
+      .withCssClass('g-col-xxl-7'),
     cx.cssClass
       .withLabel('8/12')
-      .withCssClass('g-col-xl-8'),
+      .withCssClass('g-col-xxl-8'),
     cx.cssClass
       .withLabel('9/12')
-      .withCssClass('g-col-xl-9'),
+      .withCssClass('g-col-xxl-9'),
     cx.cssClass
       .withLabel('10/12')
-      .withCssClass('g-col-xl-10'),
+      .withCssClass('g-col-xxl-10'),
     cx.cssClass
       .withLabel('11/12')
-      .withCssClass('g-col-xl-11'),
+      .withCssClass('g-col-xxl-11'),
     cx.cssClass
       .withLabel('12/12')
-      .withCssClass('g-col-xl-12'),
+      .withCssClass('g-col-xxl-12'),
     );
