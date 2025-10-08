540a16fec86d7c6927e6c5c2aa4c0135deafe58f
kno <keri.nogarth@bsi-software.com>
2025-10-08 07:45:43 +0200

    refactor: cleaup text editor

--- configs/editor/full.js
+++ configs/editor/full.js
@@ -9,20 +9,11 @@ module.exports = cx.htmlEditorConfig
     Feature.STRIKE_THROUGH,
     Feature.SUBSCRIPT,
     Feature.SUPERSCRIPT,
-    Feature.FONT_SIZE,
-    Feature.LINE_HEIGHT,
     Feature.TEXT_COLOR,
-    Feature.BACKGROUND_COLOR,
-    Feature.ALIGN_LEFT,
-    Feature.ALIGN_CENTER,
-    Feature.ALIGN_RIGHT,
     Feature.ALIGN_JUSTIFY,
     Feature.FORMAT_OL,
     Feature.FORMAT_UL,
-    Feature.OUTDENT,
-    Feature.INDENT,
-    Feature.PARAGRAPH_FORMAT,
-    Feature.QUOTE,
+    // Feature.PARAGRAPH_FORMAT,
     Feature.SPECIAL_CHARACTERS,
     Feature.EMOTICONS,
     Feature.INSERT_LINK,
@@ -34,10 +25,8 @@ module.exports = cx.htmlEditorConfig
     Feature.HTML,
     Feature.HELP)
   .withTextColors(bsiProperty('textColor', '#383e42'), bsiProperty('primaryColor', '#2F4E66'), bsiProperty('secondaryColor', '#93C120'), bsiProperty('lightColor', '#ffffff'), bsiProperty('darkColor', '#000000'))
-  .withBackgroundColors(bsiProperty('primaryColor', '#2F4E66'), bsiProperty('secondaryColor', '#93C120'), bsiProperty('lightColor', '#ffffff'), bsiProperty('darkColor', '#000000'))
-  .withFormats(Format.P, Format.H1, Format.H2, Format.H3, Format.H4, Format.H5, Format.H6, Format.PRE)
+  // .withFormats(Format.P, Format.H1, Format.H2, Format.H3, Format.H4, Format.H5, Format.H6, Format.PRE)
   .withFontSizes(8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 72)
   .withFontSizeUnit(FontSizeUnit.PX)
   .withFontSizeDefault(16)
-  .withLineHeights(1, 1.15, 1.5, 2)
   .withEnterMode(EnterMode.P);
