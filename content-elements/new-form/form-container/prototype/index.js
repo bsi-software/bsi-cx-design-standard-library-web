eba47de8a978408a9c1c941f9824161ba7a602db
kno-bsi <keri.nogarth@bsi-software.com>
2025-11-21 14:24:53 +0100

    WIP

--- content-elements/new-form/form-container/prototype/index.js
+++ content-elements/new-form/form-container/prototype/index.js
@@ -9,22 +9,20 @@ const { cx, Icon } = require("@bsi-cx/design-build");
  * @returns {ContentElement}
  */
 module.exports = (
-  template = require("../template.twig"),
+  template = require("./template.twig"),
   elementId = "form-container-84c6581f",
   elementLabel = "Neues Formular",
   elementDescription = "Element Beschreibung",
   dropzoneId = "form-container-dropzone-cd1f258c"
 ) => cx
   .contentElement
-  .withFile(require(template))
+  .withFile(template)
   .withElementId(elementId)
   .withLabel(elementLabel)
   .withDescription(elementDescription)
   .withIcon(Icon.ONE_COLUMN)
   .withStyleConfigs(
-    require("../../../../configs/styles/form-color-scheme.js"),
     require("../../../../configs/styles/form-button-alignment.js"),
-
   )
   .withParts(
     cx.part.form
---
+++ content-elements/new-form/form-elements/form-fields/text-field/prototype/index.js
@@ -0,0 +1,37 @@
+const { cx, Icon } = require("@bsi-cx/design-build");
+
+/**
+ * @param {string} template
+ * @param {string} elementId
+ * @param {string} elementLabel
+ * @param {string} elementDescription
+ * @param {string} formFieldPartLabel
+ * @param {string} formFieldPartId
+ * @param {string} helperTextPartLabel
+ * @param {string} helperTextPartId
+ * @param {string} errorTextPartLabel
+ * @param {string} errorTextPartId
+ * @returns {ContentElement}
+ */
+module.exports = (
+  template = require("../template.twig"),
+  elementId = "form-field-2217aee4",
+  elementLabel = "Text Input",
+  elementDescription = "Element Beschreibung",
+  formFieldPartLabel = "Formularfeld: Text",
+  formFieldPartId = "form-field-part-740b4e74",
+  helperTextPartLabel = "Helper Text",
+  helperTextPartId = "form-field-part-text-fb1cdc8d",
+  errorTextPartLabel = "Fehlermeldung bei ungültiger Eingabe",
+  errorTextPartId = "form-field-part-error-invalid-21769f4a",
+) => cx.contentElement
+  .withFile(template)
+  .withElementId(elementId)
+  .withDescription(elementDescription)
+  .withLabel(elementLabel)
+  .withIcon(Icon.TEXT)
+  .withParts(
+    cx.part.FormField(formFieldPartLabel, formFieldPartId),
+    cx.part.PlainText(helperTextPartLabel, helperTextPartId),
+    cx.part.PlainText(errorTextPartLabel, errorTextPartId),
+  );
