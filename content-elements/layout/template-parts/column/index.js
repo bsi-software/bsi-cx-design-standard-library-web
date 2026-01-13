51c345942af66f5904b5c2300b9d8d2a9dc139f5
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-13 09:33:31 +0100

    remove context file from layout elements

--- content-elements/layout/template-parts/column/index.js
+++ content-elements/layout/template-parts/column/index.js
@@ -10,10 +10,9 @@ module.exports = cx.templateElement
   .withLabel("Spalte (Grid) (Template Part)")
   .withFile(require("./template.hbs.twig"))
   .withIcon(Icon.ONE_COLUMN)
-  .withRawContextFile(require("./context.json"))
   .withTemplateParts(
     // Tooltip: Gibt an, ob die Splaten abgerundete Ecken haben oder nicht. Dies hat nur auswirkungen auf Spalten, die keinen transparenten hintergrund haben.
-    cx.templatePart.Checkbox("Abgerundete Ecken? (nur bei nicht transparentem Hintergrund relevant)", "column-rounded-corners-J2lsXv"),
+    cx.templatePart.Checkbox("Abgerundete Ecken? (nur bei nicht transparentem Hintergrund relevant)", "column-rounded-corners-J2lsXv").withCheckboxPrefill(true),
     // Tooltip: Gibt die Hintergundfarbe der Spalte an.
     cx.templatePart.Option("Hintergrundfarbe der Spalte", "column-background-color-XzF28g", {
         "Transparent" : "bg-color-transparent",
@@ -22,26 +21,26 @@ module.exports = cx.templateElement
         "Tertiary" : "bg-color-tertiary",
         "Light": "bg-color-light",
         "Dark" : "bg-color-dark",
-    }),
+    }).withOptionPrefill("bg-color-transparent"),
     // Tooltip: Bestimmt, wie die Elemente innerhalb der Spalte horizontal angeordnert werden.
     cx.templatePart.Option("Elemente horizontal ausrichten", "column-horizontal-alignment-2AmT0r", {
         "Links" : "column-horizontal-alignment-left",
         "Zentriert" : "column-horizontal-alignment-center",
         "Rechts" : "column-horizontal-alignment-right",
-    }),
+    }).withOptionPrefill("column-horizontal-alignment-left"),
     // TODO: When complexity levels are introduced, move this code to the appropriate complexity layer.
     // Tooltip: Legt fest, wie die Elemente innerhalb in der Höhe der Spalte positioniert werden.
     cx.templatePart.Option("Elemente vertikal ausrichten", "column-vertical-alignment-LuY0Fn", {
         "Oben" : "column-vertical-alignment-top",
         "Mittig" : "column-vertical-alignment-center",
         "Unten" : "column-vertical-alignment-bottom",
-    }),
+    }).withOptionPrefill("column-vertical-alignment-top"),
     // TODO: When complexity levels are introduced, move this code to the appropriate complexity layer.
     // Tooltip: Bestimmt, ob die Elemente in der Spalte untereinander oder nebeneinander angeordnet sind.
     cx.templatePart.Option("Anordnung der Elemente", "column-direction-YjtUbZ", {
         "Untereinander" : "column-direction-vertical",
         "Nebeneinander" : "column-direction-horizontal",
-    }),
+    }).withOptionPrefill("column-direction-vertical"),
   )
   .withDropzones(
     cx.dropzone
--- content-elements/layout/template-parts/grid/index.js
+++ content-elements/layout/template-parts/grid/index.js
@@ -10,23 +10,22 @@ module.exports = cx.templateElement
   .withLabel("Row (Template Part)")
   .withFile(require("./template.hbs.twig"))
   .withIcon(Icon.THREE_COLUMNS)
-  .withRawContextFile(require("./context.json"))
   .withTemplateParts(
-    cx.templatePart.Checkbox("Hintergrund volle Breite?", "grid-expand-background-7h1Mpz"),
+    cx.templatePart.Checkbox("Hintergrund volle Breite?", "grid-expand-background-7h1Mpz").withCheckboxPrefill(false),
     cx.templatePart.Option("Spalten Verteilung", "grid-template-XepNDf", {
         "auto" : "grid-template-auto",
         "1 Spalte" : "grid-template-1",
         "1:1" : "grid-template-1-1",
         "2:1" : "grid-template-2-1",
         "1:2" : "grid-template-1-2",
-    }),
+    }).withOptionPrefill("grid-template-1"),
     cx.templatePart.Option("Hintergrundfarbe der Row", "grid-background-color-cN3IpC", {
         "Transparent" : "bg-color-transparent",
         "Primary" : "bg-color-primary",
         "Secondary" : "bg-color-secondary",
         "Tertiary" : "bg-color-tertiary",
         "Dark" : "bg-color-dark",
-    }),
+    }).withOptionPrefill("bg-color-transparent"),
   )
   .withDropzones(
     cx.dropzone
