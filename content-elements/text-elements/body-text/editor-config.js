2d49f584530b0d6bc03ed074c8c3320283d3c99d
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-16 13:37:22 +0100

    add textelement

--- content-elements/text-elements/body-text/editor-config.js
+++ content-elements/text-elements/body-text/editor-config.js
@@ -6,8 +6,21 @@ module.exports = cx.htmlEditorConfig
     Feature.BOLD,
     Feature.ITALIC,
     Feature.UNDERLINE,
+    Feature.FONT_SIZE,
+    Feature.LINE_HEIGHT,
+    Feature.TEXT_COLOR,
+    Feature.ALIGN_LEFT,
+    Feature.ALIGN_CENTER,
+    Feature.ALIGN_RIGHT,
+    Feature.ALIGN_JUSTIFY,
     Feature.FORMAT_OL,
     Feature.FORMAT_UL,
     Feature.INSERT_LINK,
     Feature.HELP)
+  // INFO: Colors must be specified as HEX values here because Bootstrap variables are not available in this context.
+  .withTextColors('#282322', '#ffffff', '#0082a1', '#fe9915', '#2bb198')
+  .withFontSizes(12, 14, 16, 24, 36, 48, 72)
+  .withFontSizeDefault(16)
+  .withLineHeights(1, 1.15, 1.5, 2)
   .withEnterMode(EnterMode.P);
+;
