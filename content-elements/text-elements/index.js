e34f4eb2d859de2c8b5bb90224898dd0af045653
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-23 11:41:26 +0100

    add headline

--- content-elements/text-elements/index.js
+++ content-elements/text-elements/index.js
@@ -2,11 +2,12 @@ const { cx } = require('@bsi-cx/design-build');
 
 const textElements = [
     require("./body-text"),
+    require("./headlines/h1"),
 ];
 
 module.exports.textElements = textElements;
 module.exports.textGroup = cx.contentElementGroup
-  .withGroupId('text-elements-SFq5xX')
+  .withGroupId('text-elements')
   .withLabel('Text Elemente')
   .withContentElements(
     ...textElements
