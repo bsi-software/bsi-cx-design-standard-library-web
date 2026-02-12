07d6bec4a5e31eef305c083e0b608bf4638036d5
Benni Vier <benjamin.vier@bsi-software.com>
2026-02-12 16:20:14 +0100

    Eslint light

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import jsdoc from "eslint-plugin-jsdoc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+let ignoreOldElements = ["**/_old-content-elements/**", "**/styles/**"];
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ignoreOldElements,
+    plugins: { js, jsdoc },
+    extends: [
+      "js/recommended",
+      jsdoc.configs["flat/recommended"],
+      eslintConfigPrettier,
+    ],
+    languageOptions: { globals: { ...globals.browser, ...globals.commonjs } },
+  },
+  {
+    files: ["**/*.js"],
+    ignores: ignoreOldElements,
+  },
+  {
+    files: ["**/*.json"],
+    ignores: ["package-lock.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+]);
