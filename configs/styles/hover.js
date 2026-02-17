b9ba1354cc773c4c25a931dbaa684660e6d89050
mima-bsi <Miriana.Martini@bsi-software.com>
2026-02-17 14:56:08 +0100

    BSI-426096: shadow configuration issue and add new hover configuration style for table
    Ticket: #426096
    
    Trial: 25.3.5
    Design: [MIMA] #426096 Table Style
    Content: [MIMA]  #426096 Table Style Test
    
    ---------
    
    Co-authored-by: derB4 <122090401+derB4@users.noreply.github.com>

---
+++ configs/styles/hover.js
@@ -0,0 +1,14 @@
+const {cx} = require('@bsi-cx/design-build');
+
+module.exports = cx.style
+  .withIdentifier('hover-mW41Ti')
+  .withLabel('Hover')
+  .withCssClasses(
+    cx.cssClass
+      /*.withLabel('Hide Hover')*/
+      .withLabel('Kein Hervorheben')
+      .withCssClass('bsi-table-hover-hide'),
+    cx.cssClass
+      /*.withLabel('Show Hover')*/
+      .withLabel('Hervorheben')
+      .withCssClass('bsi-table-hover'));
