46b215cd7795a00646382b2c9392bb8ee5442238
Lukas Heyer <lukas.heyer@bsi-software.com>
2026-03-11 08:23:37 +0100

    check formatter

--- content-elements/buttons/translations-de.js
+++ content-elements/buttons/translations-de.js
@@ -14,20 +14,20 @@
 //   all user-facing strings should be defined here.
 // ------------------------------------------------------------
 
-module.exports = {
+module.exports =       {
   buttons: {
     label: "Button gefüllt (Template Part)",
     roundedCorners: "Abgerundete Ecken?",
     fullWidth: "Soll der Button die ganze Breite einnehmen?",
     iconLabel: "Icon",
     // TODO: Übersetzung ist hier vorbereitet, aber kann nicht genutzt werden momentan -> Lösung finden
-    icons: {
-      noIcon: "Kein Icon",
-      chevronLeft: "Chevron links vom Text",
-      chevronRight: "Chevron rechts vom Text",
-      arrowLeft: "Pfeil links vom Text",
-      arrowRight: "Pfeil rechts vom Text",
-    },
+            icons: {
+              noIcon: "Kein Icon",
+              chevronLeft: "Chevron links vom Text",
+              chevronRight: "Chevron rechts vom Text",
+              arrowLeft: "Pfeil links vom Text",
+              arrowRight: "Pfeil rechts vom Text",
+            },
     bfsgLabel: "Dies ist ein Button, der einen Link beinhaltet",
     backgroundColorLabel: "Hintergrundfarbe des Buttons",
     // TODO: Übersetzung ist hier vorbereitet, aber kann nicht genutzt werden momentan -> Lösung finden
