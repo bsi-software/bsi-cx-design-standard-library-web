a1700d57c273c127f41c3b8fd359114634f54519
derB4 <122090401+derB4@users.noreply.github.com>
2026-07-10 08:36:40 +0200

    SL2: Textelemente (#239)
    Co-authored-by: kno-bsi <keri.nogarth@bsi-software.com>

---
+++ content-elements/text-elements/index.js
@@ -0,0 +1,9 @@
+const { cx } = require("@bsi-cx/design-build");
+
+const textElements = [require("./body-text"), require("./headlines/h1")];
+
+module.exports.textElements = textElements;
+module.exports.textGroup = cx.contentElementGroup
+  .withGroupId("text-elements")
+  .withLabel("Text Elemente")
+  .withContentElements(...textElements);
