4af577ce409fc5c546d9cd6711d8a1126629c964
Antonino Del Fiume <antonino.delfiume@bsi-software.com>
2026-09-03 10:11:08 +0200

    Add ariadescribed by on inputs

--- content-elements/form/checkbox/prototype/checkbox.js
+++ content-elements/form/checkbox/prototype/checkbox.js
@@ -1,11 +1,26 @@
 import Alpine from "@alpinejs/csp";
 
 Alpine.data("checkbox", () => ({
+  tooltip: null,
+  init() {
+    const form = this.$root.querySelector("input").closest('.bsi-element-form-container-692qIu');
+    this.tooltip = form.classList.contains('bsi-form-info-as-tooltip') ?  this.$refs.fieldTooltip : this.$refs.infoText;
+    const inputId = this.$root.querySelector("input")?.id;
+    this._setTooltipAccessibility(inputId);
+  },
+  _setTooltipAccessibility(inputId) {
+    if (inputId) {
+      this.tooltip.id = `${inputId}-tooltip`;
+      this.$root
+        .querySelector(`#${inputId}`)
+        .setAttribute("aria-describedby", this.tooltip.id);
+    }
+  },
   validateCheckbox() {
     this.$el.setAttribute("aria-invalid", !this.$el.checkValidity());
     // not required or required and valid
     if (this.$el.checkValidity()) {
-      this.$el.removeAttribute("aria-describedby");
+       this._setTooltipAccessibility(this.$el.id);
     }
     // required and invalid
     else if ("ariaDescribedByElements" in Element.prototype) {
